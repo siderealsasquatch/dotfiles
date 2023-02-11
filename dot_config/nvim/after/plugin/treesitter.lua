0d2d88eeb88681deed72ceeb3b012f206d8f12e0
Fahmi Adi Nugraha <fnugraha@gmail.com>
2023-02-11 10:27:01 +0700

    Make telecope keybinds work, add TS parsers, and modify some configs.
    The other configs being those for lazygit and git itself. These are just
    small changes about how to call delta.

--- dot_config/nvim/after/plugin/treesitter.lua
+++ dot_config/nvim/after/plugin/treesitter.lua
@@ -1,17 +1,34 @@
-require("nvim-treesitter.configs").setup {
-  ensure_installed = {"help", "javascript", "typescript", "lua", "bash", "go", "python"},
-
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-
-  highlight = {
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
-  matchup = {
-      enable = true
-  }
-}
+require("nvim-treesitter.configs").setup({
+	ensure_installed = {
+		"help",
+		"make",
+		"ini",
+		"toml",
+		"yaml",
+		"json",
+		"markdown",
+		"nix",
+		"sql",
+		"html",
+		"css",
+		"javascript",
+		"typescript",
+		"lua",
+		"bash",
+		"go",
+		"python",
+		"r",
+	},
+	-- Install parsers synchronously (only applied to `ensure_installed`)
+	sync_install = false,
+	highlight = {
+		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+		-- Using this option may slow down your editor, and you may see some duplicate highlights.
+		-- Instead of true it can also be a list of languages
+		additional_vim_regex_highlighting = false,
+	},
+	matchup = {
+		enable = true,
+	},
+})
