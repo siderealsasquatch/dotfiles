ef1330e42d9f6ecba2acde49ace60ddff5dfa1d2
Fahmi Adi Nugraha <fnugraha@gmail.com>
2023-02-16 10:47:50 +0700

    Change keybindings for Telescope and Floaterm.
    Telescope keybinds now start with "s" while floaterm keybinds start with
    "f".

--- dot_config/nvim/after/plugin/floaterm.lua
+++ dot_config/nvim/after/plugin/floaterm.lua
@@ -7,31 +7,31 @@ local map = vim.keymap.set
 g.floaterm_title = ""
 
 -- Open new terminal
-map("t", "<Leader>tn", "<C-\\><C-n>:FloatermNew --height=0.8 --width=0.8 --autoclose=2<CR>") -- open new terminal
-map("n", "<Leader>tn", ":FloatermNew --height=0.8 --width=0.8 --autoclose=2<CR>") -- open new terminal
+map("t", "<Leader>fn", "<C-\\><C-n>:FloatermNew --height=0.8 --width=0.8 --autoclose=2<CR>") -- open new terminal
+map("n", "<Leader>fn", ":FloatermNew --height=0.8 --width=0.8 --autoclose=2<CR>") -- open new terminal
 
 -- Toggle current terminal
-map("t", "<leader>tt", "<C-\\><C-n>:FloatermToggle<CR>") -- toggle terminal
-map("n", "<Leader>tt", ":FloatermToggle <CR>")
+map("t", "<leader>ft", "<C-\\><C-n>:FloatermToggle<CR>") -- toggle terminal
+map("n", "<Leader>ft", ":FloatermToggle <CR>")
 
 -- Cycle through terminals
-map("t", "<leader>t[", "<C-\\><C-n>:FloatermPrev<CR>") -- switch terminals
-map("n", "<leader>t[", ":FloatermPrev<CR>")
-map("t", "<leader>t]", "<C-\\><C-n>:FloatermNext<CR>")
-map("n", "<leader>t]", ":FloatermNext<CR>")
+map("t", "<leader>f[", "<C-\\><C-n>:FloatermPrev<CR>") -- switch terminals
+map("n", "<leader>f[", ":FloatermPrev<CR>")
+map("t", "<leader>f]", "<C-\\><C-n>:FloatermNext<CR>")
+map("n", "<leader>f]", ":FloatermNext<CR>")
 
 -- Kill terminals
-map("t", "<Leader>td", "<C-\\><C-n>:FloatermKill<CR>") -- kill current terminal
-map("n", "<Leader>td", ":FloatermKill<CR>")
-map("t", "<Leader>td", "<C-\\><C-n>:FloatermKill<CR>") -- kill all terminals
-map("n", "<Leader>tD", ":FloatermKill!<CR>")
+map("t", "<Leader>fd", "<C-\\><C-n>:FloatermKill<CR>") -- kill current terminal
+map("n", "<Leader>fd", ":FloatermKill<CR>")
+map("t", "<Leader>fd", "<C-\\><C-n>:FloatermKill<CR>") -- kill all terminals
+map("n", "<Leader>fD", ":FloatermKill!<CR>")
 
 -- Send lines to current terminal
 map("v", "<C-c><C-c>", ":'<,'>FloatermSend<CR>") -- current selection
 map("n", "<C-c><C-x>", ":%FloatermSend<CR>")
 
 -- Open LazyGit in a new terminal
-map("n", "<leader>tg", ":FloatermNew --height=0.8 --width=0.8 --autoclose=2 lazygit<CR>")
+map("n", "<leader>fg", ":FloatermNew --height=0.8 --width=0.8 --autoclose=2 lazygit<CR>")
 
 -- Open Ranger in a new terminal
-map("n", "<leader>tr", ":FloatermNew --height=0.8 --width=0.8 --autoclose=2 ranger<CR>")
+map("n", "<leader>fr", ":FloatermNew --height=0.8 --width=0.8 --autoclose=2 ranger<CR>")
