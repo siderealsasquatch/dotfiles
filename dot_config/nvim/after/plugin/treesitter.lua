fdb4e7eb7288c5cafc6c4b44b68324373917d3b1
Fahmi Adi Nugraha <fnugraha@gmail.com>
2023-04-04 15:57:01 +0700

    Enable syntax highlighting with treesitter.

--- dot_config/nvim/after/plugin/treesitter.lua
+++ dot_config/nvim/after/plugin/treesitter.lua
@@ -10,15 +10,10 @@ require("nvim-treesitter.configs").setup({
 		"markdown",
 		"markdown_inline",
 	},
-
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
-
 	highlight = {
-		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-		-- Using this option may slow down your editor, and you may see some duplicate highlights.
-		-- Instead of true it can also be a list of languages
+		enable = true,
 		additional_vim_regex_highlighting = false,
 	},
 	matchup = {
