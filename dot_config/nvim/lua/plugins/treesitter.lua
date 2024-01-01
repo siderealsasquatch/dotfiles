8bf5c56b7301ecdde01587b786ddd087b3a3d548
Fahmi Adi Nugraha <fnugraha@gmail.com>
2024-01-01 22:01:44 +0700

    Update tree-sitter config to use incremental select.

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -39,6 +39,18 @@ return {
 				enable = true, -- Must be set in order for highlighting to take effect
 				additional_vim_regex_highlighting = false,
 			},
+			indent = {
+				enable = true,
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<leader>t",
+					node_incremental = "<leader>t",
+					scope_incremental = "<leader>ts",
+					node_decremental = "<leader>td",
+				},
+			},
 			matchup = {
 				enable = true,
 			},
