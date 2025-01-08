4f5a17ca4687495cf171bc69218ac97e181e18a6
Fahmi Adi Nugraha <fnugraha@gmail.com>
2025-01-08 08:50:49 +0700

    Add colorful-menu plugin to NeoVim.

--- dot_config/nvim/lua/plugins/lsp/lsp_autocomplete.lua
+++ dot_config/nvim/lua/plugins/lsp/lsp_autocomplete.lua
@@ -5,37 +5,51 @@ return {
 	dependencies = {
 		{ "L3MON4D3/LuaSnip", version = "v2.*" },
 		"rafamadriz/friendly-snippets",
+		"xzbdmw/colorful-menu.nvim",
 	},
-	opts = {
-		keymap = {
-			preset = "default",
-		},
-		appearance = {
-			use_nvim_cmp_as_default = true,
-			nerd_font_variant = "mono",
-		},
-		signature = { enabled = true },
-		sources = {
-			default = { "lsp", "path", "snippets" },
-		},
-		completion = {
-			menu = {
-				draw = { treesitter = { "lsp" } },
+	config = function()
+		local colorful_menu = require("colorful-menu")
+		colorful_menu.setup()
+		require("blink.cmp").setup({
+			keymap = {
+				preset = "default",
 			},
-		},
-		snippets = {
-			expand = function(snippet)
-				require("luasnip").lsp_expand(snippet)
-			end,
-			active = function(filter)
-				if filter and filter.direction then
-					return require("luasnip").jumpable(filter.direction)
-				end
-				return require("luasnip").in_snippet()
-			end,
-			jump = function(direction)
-				require("luasnip").jump(direction)
-			end,
-		},
-	},
+			appearance = {
+				use_nvim_cmp_as_default = true,
+				nerd_font_variant = "mono",
+			},
+			signature = { enabled = true },
+			sources = {
+				default = { "lsp", "path", "snippets" },
+			},
+			completion = {
+				menu = {
+					draw = {
+						treesitter = { "lsp" },
+						columns = { { "kind_icon" }, { "label", "label_description", gap = 1 } },
+						components = {
+							label = {
+								text = colorful_menu.blink_components_text,
+								highlight = colorful_menu.blink_components_highlight,
+							},
+						},
+					},
+				},
+			},
+			snippets = {
+				expand = function(snippet)
+					require("luasnip").lsp_expand(snippet)
+				end,
+				active = function(filter)
+					if filter and filter.direction then
+						return require("luasnip").jumpable(filter.direction)
+					end
+					return require("luasnip").in_snippet()
+				end,
+				jump = function(direction)
+					require("luasnip").jump(direction)
+				end,
+			},
+		})
+	end,
 }
