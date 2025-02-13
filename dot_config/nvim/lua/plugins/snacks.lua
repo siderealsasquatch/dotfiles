f0144416d787cb5030d023e7a3ed2b6abfbc0f96
Fahmi Adi Nugraha <fnugraha@gmail.com>
2025-02-13 20:38:41 +0700

    Actually add the snacks.lua file

---
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,85 @@
+local snacks_config = require("snacks_config")
+
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		-- indent = snacks_config.indent_cfg,
+		picker = snacks_config.picker_cfg,
+		statuscolumn = { enabled = true },
+	},
+	keys = {
+		{
+			"<leader>ff",
+			function()
+				Snacks.picker.files()
+			end,
+			desc = "Find Files",
+		},
+		{
+			"<leader>fg",
+			function()
+				Snacks.picker.grep()
+			end,
+			desc = "Grep",
+		},
+		{
+			"<leader>fw",
+			function()
+				Snacks.picker.grep_word()
+			end,
+			desc = "Visual selection or word",
+			mode = { "n", "x" },
+		},
+		{
+			"<leader>fb",
+			function()
+				Snacks.picker.buffers()
+			end,
+			desc = "Buffers",
+		},
+		{
+			"<leader>fz",
+			function()
+				Snacks.picker.lines()
+			end,
+			desc = "Buffer Lines",
+		},
+		{
+			"<leader>fm",
+			function()
+				Snacks.picker.marks()
+			end,
+			desc = "Marks",
+		},
+		{
+			"<leader>fj",
+			function()
+				Snacks.picker.jumps()
+			end,
+			desc = "Jumps",
+		},
+		{
+			"<leader>fl",
+			function()
+				Snacks.picker.loclist()
+			end,
+			desc = "Locations",
+		},
+		{
+			"<leader>fq",
+			function()
+				Snacks.picker.qflist()
+			end,
+			desc = "Quickfix List",
+		},
+		{
+			"<leader>flO",
+			function()
+				Snacks.picker.lsp_symbols()
+			end,
+			desc = "LSP Symbols",
+		},
+	},
+}
